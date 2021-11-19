44cfe27507b3eac3bd5c95f5e9fa37a3af54d182
Alejandro Ortiz <aleortbas@gmail.com>
2021-11-19 00:04:38 -0500

    calendario

--- app/src/main/java/com/example/footboocking/formatoReserva.java
+++ app/src/main/java/com/example/footboocking/formatoReserva.java
@@ -3,12 +3,15 @@ package com.example.footboocking;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TimePicker;
 import android.widget.Toast;
@@ -21,20 +24,24 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 import java.util.Locale;
 
 public class formatoReserva extends AppCompatActivity {
 
+    private DatePickerDialog datePickerDialog;
     EditText idCancha, nombreReserva, fecha, horaInicio, horaFinal;
-    Button registrar, atras, consultar, timeButton;
+    Button registrar, atras, consultar, timeButton, dateButton;
     int id, horas, minutos;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formato_reserva);
+
+        initDatePicker();
+
 
         Intent intent = getIntent();
         id = intent.getIntExtra("id_local", 0);
@@ -49,6 +56,9 @@ public class formatoReserva extends AppCompatActivity {
         atras = findViewById(R.id.consultar);
         consultar = findViewById(R.id.consultar);
         timeButton = findViewById(R.id.timeButton);
+        dateButton = findViewById(R.id.calendarButton);
+
+        fecha   .setText(getTodayDate());
 
         consultar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -67,6 +77,73 @@ public class formatoReserva extends AppCompatActivity {
         idCancha.setText(""+id);
     }
 
+    private String getTodayDate() {
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        month = month + 1;
+        int day= cal.get(Calendar.DAY_OF_MONTH);
+        return makeDateString(day, month, year);
+    }
+
+    private void initDatePicker() {
+        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                month = month + 1;
+                String date  = makeDateString(dayOfMonth, month, year);
+                fecha.setText(date);
+            }
+        };
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day= cal.get(Calendar.DAY_OF_MONTH);
+
+        int style = AlertDialog.THEME_HOLO_DARK;
+
+        datePickerDialog = new DatePickerDialog(this, style, dateSetListener, year, month, day);
+
+    }
+
+    private String makeDateString(int dayOfMonth, int month, int year) {
+        return  getMonthFormat(month) + " " + dayOfMonth + " " + year;
+    }
+
+    private String getMonthFormat(int month) {
+        if(month == 1){
+            return "Enero";
+        }
+        if(month == 2){
+            return "Febrero";
+        }if(month == 3){
+            return "Marzo";
+        }if(month == 4){
+            return "Abril";
+        }if(month == 5){
+            return "Mayo";
+        }if(month == 6){
+            return "Junio";
+        }if(month == 7){
+            return "Julio";
+        }if(month == 8){
+            return "Agosto";
+        }if(month == 9){
+            return "Septiembre";
+        }
+        if(month == 10){
+            return "Octubre";
+        }
+        if(month == 11){
+            return "Noviembre";
+        }
+        if(month == 12){
+            return "Diciembre";
+        }
+        return "Enero";
+    }
+
+
     public void popTimePicker(View view) {
         TimePickerDialog.OnTimeSetListener onTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
             @Override
@@ -81,6 +158,10 @@ public class formatoReserva extends AppCompatActivity {
 
         timePickerDialog.setTitle("Hora selecionada");
         timePickerDialog.show();
+    }
+
+    public void openDatePicker(View view) {
+        datePickerDialog.show();
     }
 
     private horas consultar() throws JSONException, IOException {
