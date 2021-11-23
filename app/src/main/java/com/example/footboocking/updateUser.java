6092fede70dcaa3fa1a406b26a8035eef93b7660
Alejandro Ortiz <aleortbas@gmail.com>
2021-11-22 22:52:31 -0500

    duda

--- app/src/main/java/com/example/footboocking/updateUser.java
+++ app/src/main/java/com/example/footboocking/updateUser.java
@@ -2,13 +2,135 @@ package com.example.footboocking;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Base64;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.io.ByteArrayOutputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class updateUser extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    ImageView imageView;
+    EditText nom;
+    EditText email;
+    EditText clave;
+    EditText telefono;
+    String id, foto, emailTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_user);
+
+        id = getIntent().getExtras().getString("ID");
+        imageView=findViewById(R.id.userImg);
+        nom=findViewById(R.id.nombre);
+        email=findViewById(R.id.email);
+        clave=findViewById(R.id.Password);
+        telefono=findViewById(R.id.telefono);
+
+
+        nom.setText(id);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                EditText nom=findViewById(R.id.nombre);
+                try {
+                    Class.forName("com.mysql.jdbc.Driver");
+                    Connection con= DriverManager.getConnection("jdbc:mysql://192.168.0.18:50/footbocking","root","");
+                    Statement stmt= con.createStatement();
+                    ResultSet rs=stmt.executeQuery("SELECT * FROM usuario WHERE id='"+id+"'");
+
+                    while(rs.next()){
+                        foto=rs.getString("foto");
+                        emailTxt=rs.getString("email");
+                    }
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            imageView.setImageBitmap(convert(foto));
+                            email.setText(emailTxt);
+                        }
+                    });
+
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                } catch (SQLException throwables) {
+                    throwables.printStackTrace();
+                }
+
+
+            }
+        }).start();
+
+    }
+    public void tomarFoto(View v) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+
+                    try {
+                        Class.forName("com.mysql.jdbc.Driver");
+                        Connection con= DriverManager.getConnection("jdbc:mysql://192.168.0.18:50/footbocking","root","");
+                        Statement stmt= con.createStatement();
+                        stmt.executeUpdate("UPDATE usuario SET nombre='"+nom.getText().toString()+"', email='"+email.getText().toString()+"', telefono'"+telefono.getText().toString()+"',clave='"+clave.getText().toString()+"',foto='"+convert(imageBitmap)+"' WHERE id='"+id+"'");
+
+                    } catch (ClassNotFoundException e) {
+                        e.printStackTrace();
+                    } catch (SQLException throwables) {
+                        throwables.printStackTrace();
+                    }
+
+
+                }
+            }).start();
+
+        }
+    }
+
+    public static Bitmap convert(String base64Str) throws IllegalArgumentException
+    {
+        byte[] decodedBytes = Base64.decode(
+                base64Str.substring(base64Str.indexOf(",")  + 1),
+                Base64.DEFAULT
+        );
+
+        return BitmapFactory.decodeByteArray(decodedBytes, 0, decodedBytes.length);
+    }
+
+    public static String convert(Bitmap bitmap)
+    {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
+
+        return Base64.encodeToString(outputStream.toByteArray(), Base64.DEFAULT);
     }
 }
